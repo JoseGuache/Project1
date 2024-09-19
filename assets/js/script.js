ef0ef5f7c10563b35910b92fb65b155847717ee7
kadeem <kingkadeem11@gmail.com>
2024-09-19 18:46:30 -0400

    save changes

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+curl --request GET \
+--url 'https://youtube138.p.rapidapi.com/auto-complete/?q=desp&hl=en&gl=US' \
+--header 'x-rapidapi-host: youtube138.p.rapidapi.com' \
+--header 'x-rapidapi-key: d7fb9c3aaemsh388a0c9be405d08p1ba070jsne10eef8695fc'
