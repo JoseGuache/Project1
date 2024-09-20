517b29b306d2c10c40153927750f56054b8bd392
Jose Guache <joseguache@Joses-MacBook-Air-2.local>
2024-09-19 22:46:00 -0400

    added modal, added spotify api, added lyrics api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
-const searchBtnEl= document.querySelector("#search-btn")
+const searchBtnEl = document.querySelector("#search-btn")
 searchBtnEl.addEventListener("click", searchVideo)
-function searchVideo(){
-    const searchVideoEl= document.querySelector("#search-video")
-    const videoName= searchVideoEl.value;
+function searchVideo() {
+    const searchVideoEl = document.querySelector("#search-video")
+    const videoName = searchVideoEl.value;
+    lyricsApi(videoName)
     populateYouTubeApi(videoName)
 }
 
 async function populateYouTubeApi(params) {
-    const url = `https://youtube138.p.rapidapi.com/search/?q=${params}&hl=en&gl=US`;
+    const url = `https://spotify23.p.rapidapi.com/search/?q=${params}&type=multi&offset=0&limit=1&numberOfTopResults=5`;
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': 'd7fb9c3aaemsh388a0c9be405d08p1ba070jsne10eef8695fc',
-            'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+            'x-rapidapi-key': '65cb44869dmsh1ca8806e3e6d0bdp1c12ffjsn3f4b5362f295',
+            'x-rapidapi-host': 'spotify23.p.rapidapi.com'
         }
     };
-    
+
     try {
         const response = await fetch(url, options);
         const result = await response.json();
@@ -27,9 +28,49 @@ async function populateYouTubeApi(params) {
     }
 }
 
-function displayVideo(results){
-    const searchFrame= document.querySelector("#video-frame");
-    searchFrame.src= `https://www.youtube.com/embed/${results.contents[0].video.videoId}`
+
+
+function displayVideo(results) {
+    const searchFrame = document.querySelector("#spotifyPlayer");
+    searchFrame.src = `https://open.spotify.com/embed/track/${results.tracks.items[0].data.id}`
 }
 // populateYouTubeApi('beach boys');
 
+async function lyricsApi(params) {
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+    };
+
+    fetch(`https://api.lyrics.ovh/v1/${params}?=`, requestOptions)
+        .then((response) => response.json())
+        .then((results) => {
+            displayLyrics(results)
+            console.log(results)
+        })
+        .catch((error) => console.error(error));
+}
+
+function displayLyrics(results) {
+    const lyricsFrame = document.querySelector("#lyricsFrame")
+    lyricsFrame.innerHTML = `${results.lyrics.substring(0, 20)}
+    ...
+          <article aria-label="Modal demo" id="modal-demo"><button class="contrast">Read More</button></article>
+    `
+    const lyricContent = document.querySelector("#lyricContent")
+    lyricContent.innerHTML = results.lyrics
+    const modalEL = document.querySelector("#modal-demo")
+    modalEL.addEventListener("click", showModal)
+}
+const closeBtn = document.querySelector("#closeBtn")
+closeBtn.addEventListener('click', closeModal)
+function closeModal(){
+    const dialog = document.querySelector("#dialog")
+    dialog.removeAttribute("open")
+}
+
+
+function showModal() {
+    const dialog = document.querySelector("#dialog")
+    dialog.setAttribute("open", "open")
+}
